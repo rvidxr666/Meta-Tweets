7724dc4d5e88f4b5f160c1fff79286cc6f57ff35
maksimum232@gmail.com <maksimum232@gmail.com>
2022-01-19 23:12:09 +0100

    More Functionality

--- ArticlesScraper.py
+++ ArticlesScraper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementNotInteractableException
 from bs4 import BeautifulSoup
 import os
 import re
@@ -31,29 +32,45 @@ class ScrapingBrowser:
         tree = html.fromstring(source_page)
         return tree
 
-# def collecting_data():
+
+def articles_summary(lst, browser):
+    for pack in lst:
+        browser.driver.get("https://smmry.com/")
+        while True:
+            try:
+                browser.driver.find_element_by_xpath('//input[contains(@class, "sm_force_url_alone")]').send_keys(pack[0])
+                break
+            except ElementNotInteractableException:
+                pass
 
 
 
 def main(art_addr):
     df = pd.DataFrame()
     browser = ScrapingBrowser(art_addr)
-    try:
-        browser.driver.find_element_by_xpath('//button[contains(@class, "css-aovwtd")]').click()
-    except NoSuchElementException:
-        pass
 
     while True:
+        current_link = browser.driver.current_url
         browser.scroll_down()
+
+        lst_of_links = [link.get_attribute("href") for link in browser.driver
+            .find_elements_by_xpath('//div[contains(@class, "yuRUbf")]/a')]
+        lst_of_articles = [link.text for link in browser.driver
+            .find_elements_by_xpath('//h3[contains(@class, "LC20lb MBeuO DKV0Md")]')]
+        lst_of_lsts = [list(x) for x in zip(lst_of_links, lst_of_articles)]
+        articles_summary(lst_of_lsts, browser)
+        browser.driver.get(current_link)
+        # print(len(lst_of_lsts), lst_of_lsts)
+
         try:
-            browser.driver.find_element_by_xpath('//div[contains(@class, "css-vsuiox")]/button').click()
+            link = browser.driver.find_element_by_xpath('//a[contains(@id, "pnnext")]').get_attribute("href")
+            browser.driver.get(link)
         except NoSuchElementException:
             break
 
-    browser.driver.
-
-    browser.driver.close()
+    # browser.driver.close()
 
 
 if __name__ == "__main__":
-    main("https://www.nytimes.com/search?dropmab=false&endDate=20220119&query=Meta&sort=best&startDate=20211001")
+    main(
+        "https://www.google.com/search?q=facebook+meta+opinion&sxsrf=AOaemvIdShQ0o70xHFfLO1BMdEFeLAar3Q%3A1642626722115&ei=on7oYf6wBvKFrwT15bDoCg&oq=facebook+meta+opinion&gs_lcp=Cgdnd3Mtd2l6EAEYADIFCAAQgAQ6BwgjEOoCECc6BAgjECc6CgguEMcBENEDEEM6BQguEJECOgsILhCABBDHARDRAzoFCC4QgAQ6BAgAEEM6CwguEIAEEMcBEK8BOgUIABCRAjoKCAAQgAQQhwIQFDoHCAAQgAQQCjoFCAAQywE6CAgAEBYQChAeOgYIABAWEB46CQgAEMkDEBYQHkoFCDwSATVKBAhBGABKBAhGGABQAFifNWDBPGgGcAB4AIABdYgB9A2SAQQyMC4ymAEAoAEBsAEKwAEB&sclient=gws-wiz")
